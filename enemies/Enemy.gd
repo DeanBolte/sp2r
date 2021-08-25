395258aaa4dd8d51ea2803701e5730604a4a8420
DeanBolte <dean.bolte7@gmail.com>
2021-08-25 20:15:43 +1000

    modified enemy collisions

--- enemies/Enemy.gd
+++ enemies/Enemy.gd
@@ -5,11 +5,39 @@ export var GRAVITY = 1700
 export var JUMP_STRENGTH = 550
 export var MINIMUM_JUMP_HEIGHT = 48
 export var JUMP_DISTANCE_MODIFIER = 3 # divides the distance between the collision object
+export var ACTIVE_RANGE = 512
+
+onready var Player = get_parent().get_parent().get_node("Player")
+
+enum {
+	ACTIVE,
+	IDLE
+}
 
 var velocity = Vector2.ZERO
 var dir = 1
+var state = IDLE
 
 func _physics_process(delta):
+	match state:
+		ACTIVE:
+			move_state()
+		IDLE:
+			pass
+	
+	# detect if player is within range
+	if Player.position.x < position.x - ACTIVE_RANGE or Player.position.x > position.x + ACTIVE_RANGE:
+		state = IDLE
+	else:
+		state = ACTIVE
+	
+	# apply gravity
+	velocity.y += GRAVITY * delta
+	
+	# move character
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func move_state():
 	if is_on_floor():
 		# check for possible jump
 		var testVel = velocity
@@ -25,10 +53,3 @@ func _physics_process(delta):
 		dir *= -1
 	else:
 		velocity.x = MOVE_SPEED * dir
-	
-	
-	# apply gravity
-	velocity.y += GRAVITY * delta
-	
-	# move character
-	velocity = move_and_slide(velocity, Vector2.UP)
